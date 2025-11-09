3b30dcafae9d577a0057493e5d1090785e275cad
Sarthak <sps1695@tamu.edu>
2025-11-08 18:32:08 -0600

    databricks carry

--- ml.py
+++ ml.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np 
 df= pd.DataFrame()
 
-
-df = pd.concat([pd.read_excel(f"mai-shen-yun/{i}_Data_Matrix (1).xlsx") for i in ["May", "June", "July", "August", "September", "October"]], axis = 0)
-df.to_csv("mai-shen-yun/Data_Matrix (1).csv")
+def update():
+    a = pd.DataFrame()
+    for i in ["May", "June", "July", "August", "September", "October"]:
+        df = pd.read_excel(f"mai-shen-yun/{i}_Data_Matrix (1).xlsx")
+        df["month"] = i
+        a = pd.concat([a,df])
+    
+    a.to_csv("data/Data_Matrix (1).csv")

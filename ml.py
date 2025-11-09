1e312c58603c687b23da5297ef1b17ea4263597f
Sarthak <sps1695@tamu.edu>
2025-11-08 18:09:02 -0600

    Dashboard

--- ml.py
+++ ml.py
@@ -1,3 +1,8 @@
+
 import pandas as pd
 import numpy as np 
-import sklearn as 
+df= pd.DataFrame()
+
+
+df = pd.concat([pd.read_excel(f"mai-shen-yun/{i}_Data_Matrix (1).xlsx") for i in ["May", "June", "July", "August", "September", "October"]], axis = 0)
+df.to_csv("mai-shen-yun/Data_Matrix (1).csv")
